422d0192edf89a77d8d8ce595e8f507614e4a3dd
Your Name201~ <abolore200@gmail.com>
2023-06-05 09:18:52 +0100

    completed add-property page

---
+++ public/assets/js/addProperty/app.js
@@ -0,0 +1,9 @@
+// const form = document.querySelector('form')
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+
+//     const input = document.querySelector('.imagesProperty .Images label input[type="file"]').value
+
+//     console.log(input);
+// })
