85544ff413bbc6e9d0a85aa73ce8994dc46e33da
Your Name201~ <abolore200@gmail.com>
2023-07-06 13:26:57 +0100

    completed manage property

--- public/assets/js/manageProperty/list.js
+++ public/assets/js/manageProperty/list.js
@@ -3,27 +3,87 @@ let propertyJSON = JSON.parse(property)
 
 const display = document.querySelector('.display-property')
 
-
 let lists_of_properties = ""
 for(let i = 0; i < propertyJSON.length; i++){
     lists_of_properties += `
-        <div class="property_home">
-            <div class="property_image">
-                <img src="${propertyJSON[i]?.images?.[0]}" alt="property_image" >
+        <a href="./viewproperty.html" class="property_link">
+            <div class="property_home">
+                <div class="property_image">
+                    <img src="${propertyJSON[i]?.images?.[0]}" alt="property_image" class="img-one">
+                </div>
+                <div class="property_details">
+                    <div class="name_of_property property">
+                        <p>${(propertyJSON[i].name_of_property.length > 15 ? propertyJSON[i]?.name_of_property.substring(0, 15) + "...." : propertyJSON[i]?.name_of_property)}</p>
+                    </div>
+                    <div class="location_of_property property">
+                        <p>${propertyJSON[i]?.location_of_property}</p>
+                    </div>
+                    <div class="range_of_property property">
+                        <p>${propertyJSON[i]?.range_of_property}</p>
+                    </div>
+                </div>
             </div>
-            <div class="property_details">
-                <div class="name_of_property property">
-                    <p> ${(propertyJSON[i].name_of_property.length > 15 ? propertyJSON[i]?.name_of_property.substring(0, 15) + "...." : propertyJSON[i]?.name_of_property)} </p>
+            
+            <div class="property-block">
+                <div class="hidden-images">
+                    <img src="${propertyJSON[i]?.images?.[1]}" alt="property_image" class="img-two">
+                    <img src="${propertyJSON[i]?.images?.[2]}" alt="property_image" class="img-three">
+                    <img src="${propertyJSON[i]?.images?.[3]}" alt="property_image" class="img-four">
                 </div>
-                <div class="location_of_property property">
-                    <p> ${propertyJSON[i]?.location_of_property} </p>
-                </div>
-                <div class="range_of_property property">
-                    <p> ${propertyJSON[i]?.range_of_property} </p>
+                <div class="hidden-features_description">
+                    <p class="features">${propertyJSON[i]?.features_of_property}</p>
+                    <p class="description">${propertyJSON[i]?.description_of_property}</p>
                 </div>
             </div>
-        </div>
+        </a>
     `
 }
 
 display.innerHTML = lists_of_properties
+
+const clickProperty = document.querySelectorAll('.display-property a')
+clickProperty.forEach((lists) => {
+    lists.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        ui.getList(lists)
+
+        window.location.href = 'viewproperty.html'
+    })
+})
+
+
+class UI{
+    getList(lists){
+        const arr = {
+            name: lists.querySelector('.name_of_property p').textContent,
+            location: lists.querySelector('.location_of_property p').textContent,
+            images : {
+                image_one: lists.querySelector('.property_image .img-one').src,
+                image_two: lists.querySelector('.hidden-images .img-two').src,
+                image_three: lists.querySelector('.hidden-images .img-three').src,
+                image_four: lists.querySelector('.hidden-images .img-four').src
+            },
+            range: lists.querySelector('.range_of_property p').textContent,
+            features: lists.querySelector('.hidden-features_description .features').textContent,
+            description: lists.querySelector('.hidden-features_description .description').textContent
+        }
+
+        this.saveListToSessionStorage(arr)
+    }
+    saveListToSessionStorage(arr){
+        let app = this.getListToSessionStorage()
+        app.push(arr)
+        sessionStorage.setItem('viewProperty', JSON.stringify(app))
+    }
+    getListToSessionStorage(){
+        let app;
+        let appSS = sessionStorage.getItem('viewProperty')
+        if(appSS === null){
+            app = []
+        } else {
+            app = JSON.parse(appSS)
+        } return app
+    }
+}
+const ui = new UI()
---
+++ public/assets/js/manageProperty/view.js
@@ -0,0 +1,57 @@
+const viewproperty = sessionStorage.getItem('viewProperty')
+let propertyJSON = JSON.parse(viewproperty)
+
+// console.log(propertyJSON?.name);
+
+const display = document.querySelector('.display-property')
+
+const newDiv = document.createElement('div')
+newDiv.innerHTML = `
+    <div class="home-property">
+        <div class="name_of_property nameProperty">
+            <p>${propertyJSON[0]?.name}</p>
+        </div>
+        <div class="all_images property nameProperty">
+            <div class="big-image">
+                <img src="${propertyJSON[0]?.images?.image_one}" alt="img-one"/>
+            </div>
+            <div class="small-image">
+                <ul>
+                    <li>
+                        <div class="full-image">
+                            <img src="${propertyJSON[0]?.images?.image_two}" alt="img-two">
+                        </div>
+                    </li>
+                    <li>
+                        <div class="full-image">
+                            <img src="${propertyJSON[0]?.images?.image_three}" alt="img-three">
+                        </div>
+                    </li>
+                    <li>
+                        <div class="full-image">
+                            <img src="${propertyJSON[0]?.images?.image_four}" alt="img-four">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="features_range nameProperty">
+            <div class="location_of_property property">
+                <p>${propertyJSON[0]?.location}</p>
+            </div>
+            <div class="range_of_property property">
+                <p>${propertyJSON[0]?.range}</p>
+            </div>
+            <div class="features_of_property property">
+                <p>${propertyJSON[0]?.features}</p>
+            </div>
+        </div>
+    </div>
+`;
+
+display.appendChild(newDiv)
+
+const backArrow = document.querySelector('.back-arrow a')
+backArrow.addEventListener('click', () => {
+    sessionStorage.removeItem('viewProperty')
+})
